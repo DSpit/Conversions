fba5c0e18f3b700f39821df682749bb25c998b57
David Boivin ~Spit <db2_hockey@hotmial.com>
2014-10-09 22:45:46 -0400

    -removed all bin files -fixed RadioButton Regester issue

--- Conversions/src/com/dSpit/conversions/mainGUI/MainFrame.java
+++ Conversions/src/com/dSpit/conversions/mainGUI/MainFrame.java
@@ -1,5 +1,6 @@
 package com.dSpit.conversions.mainGUI;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,7 +14,9 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-import com.dSpit.conversions.*;
+import com.dSpit.conversions.CANtoUSA;
+import com.dSpit.conversions.Conversions;
+import com.dSpit.conversions.USAtoCAN;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JApplet{
@@ -21,10 +24,11 @@ public class MainFrame extends JApplet{
 // Constants --------------------------------------------------------------- //
 	
 	private final String CONVERT_BUTTON = "Convert";
-	private final String CONVERT_TO_CAN = "Convert from USA $ to Canadien $";
-	private final String CONVERT_TO_USA = "convert from Canadien $ to USA $";
+	private final String CONVERT_TO_CAN = "Convert from USA $ to Can $";
+	private final String CONVERT_TO_USA = "Convert from Can $ to USA $";
 	private final String INPUT_PROMPT = "Enter Amount you wish to convert: ";
 	private final String OUTPUT_PROMPT = "Result of Conversion: ";
+	private final Dimension WINDOW_DIMEN = new Dimension(300, 200);
 	private final int PADDING = 5;
 
 // Members ----------------------------------------------------------------- //
@@ -41,36 +45,37 @@ public class MainFrame extends JApplet{
 	public void init(){
 		super.init();
 		
+		this.setSize(WINDOW_DIMEN);
+		
 		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 		
 		mConvert = new JButton(CONVERT_BUTTON);
-		mToCan = new JRadioButton(CONVERT_TO_CAN, true);
+		mToCan = new JRadioButton(CONVERT_TO_CAN,true);
 		mToUSA = new JRadioButton(CONVERT_TO_USA);
 		mInput = new JTextField(10);
 		mOutput = new JLabel();
 		JLabel inputMessage = new JLabel(INPUT_PROMPT);
 		JLabel outputMessage = new JLabel(OUTPUT_PROMPT);
 		
-		JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, PADDING, PADDING));
-		inputPanel.add(inputMessage);
-		inputPanel.add(mInput);
-		
-		JPanel outputPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, PADDING, PADDING));
-		outputPanel.add(outputMessage);
-		outputPanel.add(mOutput);
-		
-		JPanel selectorPanel = new JPanel();
-		selectorPanel.setLayout(new BoxLayout(selectorPanel, BoxLayout.Y_AXIS));
-		selectorPanel.add(mToCan);
-		selectorPanel.add(mToUSA);
-		
-		
 		mConvertSelectorGroup = new ButtonGroup();
 		mConvertSelectorGroup.add(mToCan);
 		mConvertSelectorGroup.add(mToUSA);
 		
 		mConvert.addActionListener(new ConvertListener());
+		
+		JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, PADDING, PADDING));
+		inputPanel.add(inputMessage);
+		inputPanel.add(mInput);
+		
+		JPanel outputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, PADDING, PADDING));
+		outputPanel.add(outputMessage);
+		outputPanel.add(mOutput);
+		
+		JPanel selectorPanel = new JPanel();
+		selectorPanel.setLayout(new FlowLayout(FlowLayout.CENTER, PADDING, PADDING));
+		selectorPanel.add(mToCan);
+		selectorPanel.add(mToUSA);
 		
 		mainPanel.add(inputPanel);
 		mainPanel.add(selectorPanel);
@@ -109,7 +114,7 @@ public class MainFrame extends JApplet{
 				return;
 			}
 			
-			if(mConvertSelectorGroup.getSelection().equals(mToCan)){
+			if(mToCan.isSelected()){
 				conversion = new USAtoCAN();
 			}else{
 				conversion = new CANtoUSA();
